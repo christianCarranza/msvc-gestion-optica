c8ed42bb27ab6ba4acf306cdc58c121e3648de2c
Christian Carranza <carranzachristian61@gmail.com>
2022-10-15 22:07:09 -0500

    Acceso al metodo Patch CORS

--- msvc-optica-gestion-productos/src/main/java/org/multilens/msvc/optica/gestionproductos/MsvcOpticaGestionProductosApplication.java
+++ msvc-optica-gestion-productos/src/main/java/org/multilens/msvc/optica/gestionproductos/MsvcOpticaGestionProductosApplication.java
@@ -12,17 +12,17 @@ public class MsvcOpticaGestionProductosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsvcOpticaGestionProductosApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedHeaders("*")
-						.allowedMethods("*");
-			}
-		};
-	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**")
+//						.allowedOrigins("*")
+//						.allowedHeaders("*")
+//						.allowedMethods("*");
+//			}
+//		};
+//	}
 
 }
